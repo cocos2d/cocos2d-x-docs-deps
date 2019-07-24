54712c4fc9e454f897416abfe0aa9c6fe244839c
Jason Slack-Moehrle <slackmoehrle@gmail.com>
2019-07-23 22:19:26 -0400

    Deploy - uncommented out production steps

--- deploy.sh
+++ deploy.sh
@@ -53,13 +53,13 @@ echo "Deploying to web-server location"
 echo "---------------------------------"
 echo "Copy to nginx..."
 cd ~
-#rsync --recursive documentation /var/www
+rsync --recursive documentation /var/www
 
 echo "---------------------------------"
 echo "Restarting web-server..."
 echo "---------------------------------"
-#sudo /usr/sbin/nginx -s stop
-#sudo /usr/sbin/nginx
+sudo /usr/sbin/nginx -s stop
+sudo /usr/sbin/nginx
 
 echo "---------------------------------"
 echo "Deployment done!"
